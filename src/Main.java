3a2d23ac76cd28d18d1745e3bdf81b5dc6c2e40e
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-10-07 23:17:41 +0300

    Выполенена задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task2();
     }
 
     /**
      * Пусть дан произвольный список целых чисел, удалить из него четные числа
      */
     static void task1(){
-
         List<Integer> list = ArrayListGenerator(20);
         System.out.println(list);
 
@@ -31,5 +30,20 @@ public class Main {
         return list;
     }
 
-
+    /**
+     * Задан целочисленный список ArrayList.
+     * Найти минимальное, максимальное и среднее арифметическое из этого списка. (Collections.max())
+     */
+    static void task2(){
+        List<Integer> list = ArrayListGenerator(20);
+        System.out.println(list);
+        int min_value = Collections.min(list);
+        int max_value = Collections.max(list);
+        int mean_value = 0;
+        for (int i : list) {
+            mean_value += i;
+        }
+        mean_value /= list.size();
+        System.out.printf("Минимальное значение: %d\nСреднее значение: %d\nМаксимальное значение: %d%n", min_value, mean_value, max_value);
+    }
 }
