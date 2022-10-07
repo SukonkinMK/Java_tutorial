55042547374e587a2af709db9a95e59f1cae8a82
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-10-07 23:42:39 +0300

    Выполенена задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        task2();
+        task3();
     }
 
     /**
@@ -45,5 +45,62 @@ public class Main {
         }
         mean_value /= list.size();
         System.out.printf("Минимальное значение: %d\nСреднее значение: %d\nМаксимальное значение: %d%n", min_value, mean_value, max_value);
+    }
+
+    /**
+     * Реализовать алгоритм сортировки слиянием
+     */
+    static void task3(){
+        List<Integer> list = ArrayListGenerator(20);
+        System.out.println(list);
+        System.out.println(MergeSort(list,0,list.size() - 1));
+    }
+    static List<Integer> MergeSort(List<Integer> list, int start, int end){
+        if(start < end){
+            if(end - start == 1){
+                if(list.get(start) > list.get(end)){
+                    int tmp = list.get(start);
+                    list.set(start, list.get(end));
+                    list.set(end, tmp);
+                }
+            } else{
+                int mid = (start + end) / 2;
+                MergeSort(list,start,mid);
+                MergeSort(list, mid +1, end);
+                var left = start;
+                var right = mid + 1;
+                var tempArray = new int[end - start + 1];
+                var index = 0;
+                while ((left <= mid) && (right <= end))
+                {
+                    if (list.get(left) < list.get(right))
+                    {
+                        tempArray[index] = list.get(left);
+                        left++;
+                    }
+                    else
+                    {
+                        tempArray[index] = list.get(right);
+                        right++;
+                    }
+                    index++;
+                }
+                for (var i = left; i <= mid; i++)
+                {
+                    tempArray[index] = list.get(i);
+                    index++;
+                }
+                for (var i = right; i <= end; i++)
+                {
+                    tempArray[index] = list.get(i);
+                    index++;
+                }
+                for (var i = 0; i < tempArray.length; i++)
+                {
+                    list.set(start + i,tempArray[i]);
+                }
+            }
+        }
+        return list;
     }
 }
