992655f545485119091f2bed7934094995e50eca
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-10-09 12:28:50 +0300

    Выполенена задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+        System.out.println(task1(list));
     }
+
+    /**
+     * Метод, который вернет “перевернутый” список.
+     * @param list исходный список
+     * @return перевернутый список
+     */
+    static LinkedList<Integer> task1(LinkedList<Integer> list){
+        LinkedList<Integer> result = new LinkedList<>();
+        while (list.size() > 0){
+            int tmp = list.pollLast();
+            result.add(tmp);
+        }
+        return result;
+    }
+
+
 }
