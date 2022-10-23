66db7f045f2b13d41755147c1aba910e26119a96
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-10-23 13:37:52 +0300

    Создана модель человека

---
+++ src/Human.java
@@ -0,0 +1,14 @@
+public class Human {
+    private String firstName;
+    private String lastName;
+
+    public Human(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
