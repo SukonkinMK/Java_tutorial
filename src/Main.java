3f694be7d97961a2451750abf4b59cca4eab9596
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-11-13 14:02:41 +0300

    выполнена задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
+
+    public static void task1(){
+        Integer[] arr = new Integer[] {123,21,12,34,10};
+        printArr(arr);
+        Arrays.sort(arr, new MyComparator());
+        printArr(arr);
+    }
+    public static void printArr(Integer[] arr){
+        for (Integer item: arr) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+
 }
