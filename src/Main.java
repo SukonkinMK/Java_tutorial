ac7a7e2106b9e89aed158bfc45b05700f35f8682
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-12-04 10:57:36 +0300

    Выполнен пункт 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(task1());
+    }
+    public static float task1(){
+        float result;
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Введите дробное число");
+            String ans = scanner.nextLine();
+            try {
+                result = Float.parseFloat(ans);
+                return result;
+            }
+            catch (NumberFormatException | NullPointerException exception){
+                System.out.println("Ошибка ввода");
+            }
+        }
     }
 }
