1902bf733cd7c7adcde56c690f235f74363b1c92
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-12-04 11:36:13 +0300

    Выполнен пункт 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(task1());
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+        try {
+            task4();
+        }
+        catch (Exception e){
+            System.out.println("Пустые строки вводить нельзя");
+        }
+    }
+    public static void printSum(int a, int b) {
+        System.out.println(a + b);
     }
     public static float task1(){
         float result;
@@ -17,6 +39,23 @@ public class Main {
             catch (NumberFormatException | NullPointerException exception){
                 System.out.println("Ошибка ввода");
             }
+        }
+    }
+    public static void task2(){
+        int[] intArray = new int[5];
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException | IndexOutOfBoundsException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+    public static void task4() throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        String ans = scanner.nextLine();
+        if(ans.isEmpty()){
+            throw new Exception();
         }
     }
 }
